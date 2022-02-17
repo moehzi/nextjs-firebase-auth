98fe6505cb9158bf88fa2f8e1a4c12d392763d66
moehzi <faizi1234098@gmail.com>
2022-02-17 14:20:29 +0800

    feat: add form error

---
+++ components/Forms/Error.js
@@ -0,0 +1,27 @@
+import { FormHelperText } from "@mui/material";
+import { red } from "@mui/material/colors";
+
+const GetErrorMessage = (type) => {
+  switch (type) {
+    case "minLength":
+      return " Jumlah karakter tidak memenuhi minimum";
+    case "requred":
+    default:
+      return "Mohon mengisi field";
+  }
+};
+
+const FormError = ({ error }) => {
+  if (!error) {
+    return <></>;
+  }
+
+  const { type } = error;
+  const message = GetErrorMessage(type);
+
+  return (
+    <FormHelperText sx={{ color: red[500], m: 0 }}>{message}</FormHelperText>
+  );
+};
+
+export default FormError;
--- components/Pages/Home/LoginModal.js
+++ components/Pages/Home/LoginModal.js
@@ -1,73 +1,77 @@
-import { useState } from 'react'
-import Image from 'next/image'
-import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid'
-import Button from '@mui/material/Button'
-import Checkbox from '@mui/material/Checkbox'
-import Typography from '@mui/material/Typography'
-import InputAdornment from '@mui/material/InputAdornment'
-import FormControl from '@mui/material/FormControl'
-import FormHelperText from '@mui/material/FormHelperText'
-import Modal from '@mui/material/Modal'
-import TextField from '@mui/material/TextField'
+import { useState } from "react";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+import Typography from "@mui/material/Typography";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import FormError from "../../Forms/Error";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'common.white',
+  bgcolor: "common.white",
   boxShadow: 24,
   p: 4,
 };
 
 const LoginModal = ({ open, CloseModal }) => {
-  const [showPassword, setShowPassword] = useState(false)
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = () => {};
 
   return (
-    <Modal
-      open={open}
-      onClose={CloseModal}
-    >
+    <Modal open={open} onClose={CloseModal}>
       <Box sx={style}>
         <Typography variant="h4" component="h1" sx={{ mb: 4 }}>
           Sign in
         </Typography>
         <Grid sx={{ mb: 2 }}>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <FormControl sx={{ mb: 2 }} fullWidth>
               <TextField
                 id="email"
+                type="email"
                 name="email"
                 label="Email atau nomor telepon"
                 variant="filled"
-
+                {...register("email", { required: true })}
               />
-              <FormHelperText>
-                Please enter a valid email or phone number.
-              </FormHelperText>
+              <FormError error={errors.email} />
             </FormControl>
             <FormControl sx={{ mb: 4 }} fullWidth>
               <TextField
                 id="password"
                 name="password"
-                type={showPassword ? 'text' : 'password'}
+                type={showPassword ? "text" : "password"}
                 label="Password"
                 variant="filled"
                 InputProps={{
                   endAdornment: (
                     <InputAdornment position="end">
                       <Button onClick={() => setShowPassword(!showPassword)}>
-                        {showPassword ? 'Hide' : 'Show'}
+                        {showPassword ? "Hide" : "Show"}
                       </Button>
                     </InputAdornment>
-                  )
+                  ),
                 }}
+                {...register("password", { required: true, minLength: 6 })}
               />
-              <FormHelperText>
-                Your password must contain between 4 and 60 characters.
-              </FormHelperText>
+              <FormError error={errors.password} />
             </FormControl>
             <Button type="submit" variant="contained" size="large" fullWidth>
               Sign in
@@ -82,9 +86,7 @@ const LoginModal = ({ open, CloseModal }) => {
         >
           <Box>
             <Checkbox />
-            <Typography variant="caption">
-              Remember me
-            </Typography>
+            <Typography variant="caption">Remember me</Typography>
           </Box>
           <Typography variant="caption" component="a" href="#">
             Need help ?
@@ -112,7 +114,8 @@ const LoginModal = ({ open, CloseModal }) => {
         </Grid>
         <Grid>
           <Typography variant="caption">
-            This page is protected by Google reCAPTCHA to ensure you are not a bot.
+            This page is protected by Google reCAPTCHA to ensure you are not a
+            bot.
           </Typography>
           <Typography variant="caption" color="primary" component="a" href="#">
             Learn more.
@@ -120,7 +123,7 @@ const LoginModal = ({ open, CloseModal }) => {
         </Grid>
       </Box>
     </Modal>
-  )
-}
+  );
+};
 
-export default LoginModal
+export default LoginModal;
